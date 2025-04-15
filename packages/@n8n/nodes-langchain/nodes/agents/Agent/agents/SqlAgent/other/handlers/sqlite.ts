3db026902b46211fdf403a1a39bec4ed5b7c1791
joe-green123123 <joe@payperappointment.co.uk>
2025-04-15 11:36:37 +0100

    Update sqlite.ts

--- packages/@n8n/nodes-langchain/nodes/agents/Agent/agents/SqlAgent/other/handlers/sqlite.ts
+++ packages/@n8n/nodes-langchain/nodes/agents/Agent/agents/SqlAgent/other/handlers/sqlite.ts
@@ -2,7 +2,7 @@ import { DataSource } from '@n8n/typeorm';
 import * as fs from 'fs';
 import type { IExecuteFunctions, INodeExecutionData } from 'n8n-workflow';
 import { BINARY_ENCODING, NodeOperationError } from 'n8n-workflow';
-import * as sqlite3 from 'sqlite3';
+import Database from 'better-sqlite3';
 import * as temp from 'temp';
 
 export async function getSqliteDataSource(
